5bdc9064b138c51753bec4868d65e78737611838
Juan Luis Ramirez Vaquero <juanlu@Juanlu-PC>
2016-11-13 12:23:37 +0100

    Modificiado Poblar.py

--- Poblar-LDAP.py
+++ Poblar-LDAP.py
@@ -10,7 +10,7 @@ from json import loads
 # Values #
 #user = 'admin'
 conection = "ldap://localhost:389/"
-dom = 'dc=jlramirez,dc=gonzalonzareno,dc=org'
+dom = 'dc=gonzalonzareno,dc=org'
 uidNumberInitial = 2000
 gidNumber = 2000
 
@@ -19,8 +19,36 @@ passwd = getpass.getpass('Contraseña del usuario %s LDAP: ' % user)
 
 # JSON
 f = open(clase.json,'r')
-content = f.read()
+contenido = f.read()
 f.close()
-json_humans = loads(content)
-list_humans = json_humans[alumnos]
+json_clase = loads(contentenido)
+lista_clase = json_clase[alumnos]
 
+# LDAP
+try:
+	bind = "cn=%s,%s" % (user, dom)
+	l = ldap.initialize(conection)
+	l.simple_bind_s(bind,passwd)
+
+	for usuarios in lista_clase:
+		nombre = usuarios['nombre'].encode('utf8')
+		apellidos = usuarios['apellidos'].encode('utf8')
+		uid = str(usuarios['usuario'])
+		attrs = {}
+		dn = "cn=%s,%s" % (uid, dom)
+		attrs['objectClass'] = ['top', 'posixAccount', 'inetOrgPerson', 'ldapPublicKey']
+		attrs['uid'] = uid
+		attrs['cn'] = nombre
+		attrs['sn'] = apellidos
+		attrs['mail'] = str(i['correo'])
+		attrs['uidNumber'] = str(uidNumberInitial)
+		attrs['gidNumber'] = str(gidNumber)
+		attrs['homeDirectory'] = '/home/%s' % uid
+		attrs['loginShell'] = '/bin/bash'
+		attrs['sshPublicKey'] = str(i['clave'])
+		ldif = modlist.addModlist(attrs)
+		l.add_s(dn,ldif)
+		uidNumberInitial += 1
+		print 'Usuario %s insertado.' % uid 
+
+	l.unbind_s()
